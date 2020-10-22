3e29f66c9775a0cdcbe9974e3f64d16941877045
Ishaan Jain <ishaan160799@gmail.com>
2020-10-22 08:09:54 +0530

    delete post done

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "deleteBlog":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case "addBlog":
       return [
         ...state,
@@ -24,8 +26,14 @@ const addBlogPost = (dispatch) => {
   };
 };
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "deleteBlog", payload: id });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost,deleteBlogPost },
   []
 );
