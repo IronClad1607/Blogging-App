64d4480a0487d1f8e7d044667322a4d7c9d63d70
Ishaan Jain <ishaan160799@gmail.com>
2020-10-26 16:26:42 +0530

    create screen started

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import { Provider as BlogProvider } from "./src/context/BlogContext";
-import ShowScreen from "./src/screens/ShowScreen"
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
-    Show:ShowScreen,
+    Show: ShowScreen,
+    Create: CreateScreen,
   },
   {
     initialRouteName: "Index",
