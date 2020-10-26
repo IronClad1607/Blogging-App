17f8890c1366d49d675cade8136a0f4de5e3e37b
Ishaan Jain <ishaan160799@gmail.com>
2020-10-26 17:43:51 +0530

    onSubmit prop added

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -4,14 +4,19 @@ import { Context as BlogContext } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
-  
   const { addBlogPost } = useContext(BlogContext);
 
-  return <BlogPostForm/>;
+  return (
+    <BlogPostForm
+      onSubmit={(title, content) => {
+        addBlogPost(title, content, () => {
+          navigation.navigate("Index");
+        });
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
