3ccadbcde388da26c6845885e3f1b527b4a3143d
Ishaan Jain <ishaan160799@gmail.com>
2020-10-26 17:09:31 +0530

    edit screen started and show screen completed

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,19 +1,30 @@
 import React, { useContext } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { Context as BlogContext } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
 const ShowScreen = ({ navigation }) => {
   const { state } = useContext(BlogContext);
-  const blogID = navigation.getParam('id');
+  const blogID = navigation.getParam("id");
 
-  const blogPost = state.find((blogPost) => blogPost.id === blogID)
-
+  const blogPost = state.find((blogPost) => blogPost.id === blogID);
 
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
